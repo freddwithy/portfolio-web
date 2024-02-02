4f9a27a0bb2788cf3c2e8efb7aace06672c24d4f
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-02-02 17:03:01 -0300

    añadiendo modo toggle de modo oscuro

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "preact/hooks"
+
+
+export const ToggleButton = () => {
+    const [theme, setTheme] = useState(() => {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            return "dark"
+        }
+        
+        return "light"
+    })
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.querySelector('html').classList.add('dark')
+        } else {
+            document.querySelector('html').classList.remove('dark')
+        }
+    }, [theme])
+
+    const handleChangeTheme = () => {
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+    }
+
+    return (
+        <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer" onChange={handleChangeTheme}/>
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4  rounded-full dark:bg-gray-900 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet-600">
+            </div>
+        </label>
+    )
+}
+
