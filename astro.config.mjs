80eb9743b14621e29218b3ee82bfaf988d0e23e4
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-03-16 14:30:55 -0300

    varios cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +17,5 @@ export default defineConfig({
     }
   },
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
