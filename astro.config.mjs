0a9b8ab3fe7b8c65e8f8bec6de27c03ee9bda80b
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-03-18 17:24:22 -0300

    adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import node from '@astrojs/node';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://freddsana.art",
   integrations: [tailwind(), preact()],
-  build: {
-    output: "/src/assets"
-  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
-    prefixDefaultLocale: false,
-    domains: {
-      es: "https://freddsana.art",
-      en: "https://en.freddsana.art"
+    routing: {
+      prefixDefaultLocale: false,
     }
   },
-  experimental: {
-    i18nDomains: true
-  },
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
