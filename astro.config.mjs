520cbaa1197cdf9bbc868a1b28df7836fa43c139
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-03-19 10:42:45 -0300

    arreglando cosas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
